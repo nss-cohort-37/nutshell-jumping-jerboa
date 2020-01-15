e49a7f2a1c5cbaa5ba978f530a362d3778ec8ada
DanielBradleyBennett <danielbradleybennett@gmail.com>
2020-01-15 13:07:13 -0600

    login dialog

--- src/scripts/login/registerForm.js
+++ src/scripts/login/registerForm.js
@@ -7,23 +7,27 @@ const registerFormList = () => {
     contentTarget.innerHTML = 
 
     `
-    <fieldset>
-        <label for="name">Name:</label>
-        <input type="text" name="name" id="registerName__form">
-    </fieldset>
-    <fieldset>
-        <label for="username">username or email:</label>
-        <input type="text" name="username" id="registerUsername__form">
-    </fieldset>
-    <fieldset>
-        <label for="password">password:</label>
-        <input type="text" name="password" id="registerPassword__form">
-    </fieldset>
-    <fieldset>
-        <label for="password__confirm">confirm password:</label>
-        <input type="text" name="confrimPassword" id="registerConfrimPassword__form">
-    </fieldset>
-    <button id="register__button">Register Account</button>
+    <section>
+    
+        <button id="register__button">Register Account</button>
+        
+        <dialog id="registerInfo" class="registerInfo">
+            
+                <label for="name">Name:</label>
+                <input type="text" name="name" id="registerName__form">
+            
+                <label for="username">username or email:</label>
+                <input type="text" name="username" id="registerUsername__form">
+            
+                <label for="password">password:</label>
+                <input type="text" name="password" id="registerPassword__form">
+            
+                <label for="password__confirm">confirm password:</label>
+                <input type="text" name="confrimPassword" id="registerConfrimPassword__form">
+                <button class='button--close' id="button--close">Close</button>
+
+        </dialog>
+    </section>
 
     `
     
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,9 @@
 import registerFormList from "./login/registerForm.js";
 import loginFormList from "./login/loginForm.js";
+import { LoginFormButton } from "./login/dialogLogin.js";
+import { RegisterButton } from "./login/dialogRegister.js";
 
 registerFormList()
 loginFormList()
+LoginFormButton()
+RegisterButton()
