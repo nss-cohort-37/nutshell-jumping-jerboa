78efb94cd8959704ed8c0b0681e1a2ad219f1dff
tylerdav <tdavis1433@att.net>
2020-01-16 09:59:26 -0600

    added post, delete, put in providers

--- src/scripts/users/usersProvider.js
+++ src/scripts/users/usersProvider.js
@@ -1,19 +1,18 @@
+let users = [];
 
-let users = []
+export const useUsers = () => users.slice();
 
-export const useUsers = () => users.slice()
-
-export const getUsers = () => fetch("http://localhost:8088/users")
+export const getUsers = () =>
+  fetch("http://localhost:8088/users")
     .then(res => res.json())
-    .then(parsedUsers => users = parsedUsers)
+    .then(parsedUsers => (users = parsedUsers));
 
 export const saveUsers = users => {
-    return fetch('http://localhost:8088/users', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(users)
-    })
-        .then(getUsers)
-}
+  return fetch("http://localhost:8088/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(users)
+  }).then(getUsers);
+};
